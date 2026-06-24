501c696bf24d84bd64bd5688d9a295b408d3e888
Abhay Sharma <abhayunsw@gmail.com>
2026-06-24 17:04:04 +1000

    fixed homescreen event size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -127,7 +127,7 @@ export default function Home() {
         </div>
 
         {weekEvents.length > 0 ? (
-          <div className={`grid gap-8 ${weekEvents.length === 1 ? 'max-w-lg' : 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'}`}>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {weekEvents.map(event => (
               <EventCard
                 key={event.id}
